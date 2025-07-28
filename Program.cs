383c8687e50e7185615069fd22940a55aa5ba29d
RahmaElraw <rahmaelraw67@gmail.com>
2025-07-28 16:04:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace task1_CS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Input the first number:");
+            string input1=Console.ReadLine();
+            double num1;
+            while(!double.TryParse(input1,out num1))
+            {
+                Console.WriteLine("Invalid input, try again");
+                input1 = Console.ReadLine();
+            }
+
+            Console.WriteLine("Input the second number:");
+            string input2 = Console.ReadLine();
+            double num2;
+            while (!double.TryParse(input2, out num2))
+            {
+                Console.WriteLine("Invalid input, try again");
+                input2 = Console.ReadLine();
+            }
+
+            Console.WriteLine("What do you want to do with those numbers?");
+            Console.WriteLine("[A]dd");
+            Console.WriteLine("[S]ubtract");
+            Console.WriteLine("[M]ultiply");
+
+            string operation = Console.ReadLine().ToUpper();
+            switch (operation)
+            {
+                case "A":
+                    Console.WriteLine($"{num1} + {num2} = {num1+num2}");
+                    break;
+
+                case "S":
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+
+                case "M":
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to close");
+            Console.ReadLine();
+        }
+    }
+}
