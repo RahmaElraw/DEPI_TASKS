89bac68fe704c73276160e47238fef34630ef9f3
RahmaElraw <rahmaelraw67@gmail.com>
2025-08-07 17:08:59 +0300

    Add files via upload

---
+++ task2_oop/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace task2_oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BANK bank1= new BANK();
+            bank1.ShowAccountDetails();
+            Console.WriteLine("--------------------------------------");
+            BANK bank2= new BANK("Rahma ElRaw","01234567892345","01026357106","Cairo",10000);
+            bank2.ShowAccountDetails();
+        }
+    }
+}
