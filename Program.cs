2614bbe26c80f0fd5c1b7089b05b7c4181a3a8d4
RahmaElraw <rahmaelraw67@gmail.com>
2025-08-10 20:18:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,81 @@
-﻿namespace task2_oop
+﻿namespace Inheritance__Polymorphism_task
 {
     internal class Program
     {
+        // base class
+        public class BankAccount
+        {
+            public int AccountNumber { get; set; }
+            public string AccountHolder { get; set; }
+            public decimal Balance { get; set; }
+            public BankAccount(int accountNum, string accountHolder, decimal balance)
+            {
+                this.AccountNumber = accountNum;
+                this.AccountHolder = accountHolder;
+                this.Balance = balance;
+            }
+            public virtual decimal CalculateInterest()
+            {
+                return 0;
+            }
+            public virtual void ShowAccountDetails()
+            {
+                Console.WriteLine($"AccountNumber: {AccountNumber}");
+                Console.WriteLine($"AccountHolder: {AccountHolder}");
+                Console.WriteLine($"Balance: {Balance}");
+            }
+        }
+        // drived classes
+        public class SavingAccount : BankAccount
+        {
+            public decimal InterestRate { get; set; }
+            public SavingAccount(int accountNum, string accountHolder, decimal balance, decimal interestRate)
+                : base(accountNum, accountHolder, balance)
+            {
+                InterestRate = interestRate;
+            }
+            public override decimal CalculateInterest()
+            {
+                return Balance * InterestRate / 100;
+            }
+            public override void ShowAccountDetails()
+            {
+                base.ShowAccountDetails();
+                Console.WriteLine($"InterestRate: {InterestRate}");
+            }
+        }
+        public class CurrentAccount : BankAccount
+        {
+            public decimal OverdraftLimit { get; set; }
+            public CurrentAccount(int accountNum, string accountHolder, decimal balance, decimal overdraftLimit)
+                : base(accountNum, accountHolder, balance)
+            {
+                OverdraftLimit = overdraftLimit;
+            }
+            public override decimal CalculateInterest()
+            {
+                return 0;
+            }
+            public override void ShowAccountDetails()
+            {
+                base.ShowAccountDetails();
+                Console.WriteLine($"OverdraftLimit {OverdraftLimit}");
+            }
+
+        }
         static void Main(string[] args)
         {
-            BANK bank1= new BANK();
-            bank1.ShowAccountDetails();
-            Console.WriteLine("--------------------------------------");
-            BANK bank2= new BANK("Rahma ElRaw","01234567892345","01026357106","Cairo",10000);
-            bank2.ShowAccountDetails();
+            SavingAccount saving = new SavingAccount(123, "Ali", 50000, 5);
+            CurrentAccount current = new CurrentAccount(456, "Rahma", 40000, 1000);
+            List<BankAccount> accounts = new List<BankAccount> { saving, current };
+            foreach (var acc in accounts)
+            {
+                acc.ShowAccountDetails();
+                Console.WriteLine($"Calculated Interest: {acc.CalculateInterest()}");
+                Console.WriteLine("-------------------------------");
+            }
+
+
         }
     }
 }
